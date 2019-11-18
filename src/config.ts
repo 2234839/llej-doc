a0ea0cb471bb1d07f2859783d6d417e538e525e8
远程支持 <2234839456@qq.com>
2019-11-18 22:33:25 +0800

    ✨ 服务器类型判断

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
+
+const isWin = /^win/.test(process.platform)
 export const config = {
-  input_dir: './test/doc',
-  out_dir: './test/out',
+  input_dir: isWin ? './test/doc' : '',
+  out_dir: isWin ? './test/out' : '',
   /** 服务器的基本路径 */
   basePath: './',
-  template:"./template/article.html"
+  template: "./template/article.html",
+  isWin
 }
