d560eaadd5b73e88051c988edfd89a8a0b7f98e8
远程支持 <2234839456@qq.com>
2019-11-18 22:42:09 +0800

    ✨ 修改了一些权限

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 
 const isWin = /^win/.test(process.platform)
 export const config = {
-  input_dir: isWin ? './test/doc' : '',
-  out_dir: isWin ? './test/out' : '',
+  input_dir: isWin ? './test/doc' : '/root/doc/content',
+  out_dir: isWin ? './test/out' : '/root/static/doc',
   /** 服务器的基本路径 */
   basePath: './',
   template: "./template/article.html",
