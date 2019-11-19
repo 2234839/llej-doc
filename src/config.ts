c71e0d5e4c0d6929466da716e02eaded5a83162e
崮生 <admin@shenzilong.cn>
2019-11-19 22:05:33 +0800

    ✨ 新增过滤文件

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,23 @@
 const isWin = /^win/.test(process.platform);
 export const config = {
-  input_dir: isWin ? "D:/code/doc/content" : "/root/doc/content",
+  input_dir: isWin ? "D:/code/doc" : "/root/doc",
   out_dir: isWin ? "./test/out" : "/root/static/doc",
   /** 服务器的基本路径 */
   basePath: "./",
-  article_template: isWin ? "D:/code/doc/content/article.html" : "/root/doc/content/article.html",
-  menu_template: isWin ? "D:/code/doc/content/menu.html" : "/root/doc/content/menu.html",
-  footer_template: isWin ? "D:/code/doc/content/footer.html" : "/root/doc/content/footer.html",
-  header_template: isWin ? "D:/code/doc/content/header.html" : "/root/doc/content/header.html",
+  article_template: isWin ? "D:/code/doc/article.html" : "/root/doc/article.html",
+  menu_template: isWin ? "D:/code/doc/menu.html" : "/root/doc/menu.html",
+  footer_template: isWin ? "D:/code/doc/footer.html" : "/root/doc/footer.html",
+  header_template: isWin ? "D:/code/doc/header.html" : "/root/doc/header.html",
   isWin,
   /** 过滤一些目录 */
-  filter_dir: ["D:\\code\\doc\\content\\node_modules", "/root/doc/content/node_modules"],
+  filter_dir: [
+    "D:\\code\\doc\\node_modules",
+    "/root/doc/node_modules",
+    "D:\\code\\doc\\.git",
+    "/root/doc/.git",
+    "D:\\code\\doc\\.vscode",
+    "/root/doc/.vscode",
+    "D:\\code\\doc\\themes",
+    "/root/doc/themes",
+  ],
 };
