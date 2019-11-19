ce2ff546de035829644159f60f75d1f2ca72b55d
崮生 <admin@shenzilong.cn>
2019-11-19 19:16:33 +0800

    ✨ 修改平台相关

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 
 const isWin = /^win/.test(process.platform)
 export const config = {
-  input_dir: isWin ? './test/doc' : '/root/doc/content',
+  input_dir: isWin ? 'D:/code/doc/content' : '/root/doc/content',
   out_dir: isWin ? './test/out' : '/root/static/doc',
   /** 服务器的基本路径 */
   basePath: './',
