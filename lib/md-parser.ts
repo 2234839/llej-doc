1258f8e0d2b34c353cc23ec474ba676f991b2ea6
崮生 <admin@shenzilong.cn>
2019-11-19 21:31:58 +0800

    ✨ 添加attrs 模块

--- lib/md-parser.ts
+++ lib/md-parser.ts
@@ -28,7 +28,12 @@ var md = MarkdownIt({
 
 md.use(require("markdown-it-anchor")); //为标题添加id
 md.use(require("markdown-it-table-of-contents")); //[[toc]] 生成目录
-// md.use(require('markdown-it-multimd-table')) // 表格 好像对格式化后的没用
+md.use(require("markdown-it-attrs"), {
+  // {.class} 允许通过这种形式添加样式
+  leftDelimiter: "{",
+  rightDelimiter: "}",
+  allowedAttributes: [], // empty array = all attributes are allowed
+});
 /** 图片渲染 */
 const a_default_render = md.renderer.rules.a;
 md.renderer.rules.a = function(tokens, idx, options, env, self) {
